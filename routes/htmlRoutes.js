e342acf3fac80d7316e882be461c8ebcae273204
Odufa Atsegbua <odufa.atsegbua@gmail.com>
2019-11-03 19:01:07 -0600

    finished switch cases, backend fully functioning

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,9 +15,11 @@ module.exports = function(app) {
   app.get("/user/:id", function(req, res) {
     db.User.findOne({ where: { id: req.params.id } }).then(function(dbUser) {
     // res.render is what loads the info to the handlebars file (front-end)
+
+    // switch cases to make users in our database compatible
     switch(dbUser.astrology_sign){
       case "aquarius":
-      db.User.findAll({ where: { astrology_sign: ["pisces", "leo"]  }}).then(function(matches) {
+      db.User.findAll({ where: { astrology_sign: ["aries", "leo", "sagittarius"]  }}).then(function(matches) {
         // res.render is what loads the info to the handlebars file (front-end)
         res.render("user", {
           userdata: dbUser,
@@ -26,6 +28,123 @@ module.exports = function(app) {
       });
       
       break;
+
+      case "leo":
+      db.User.findAll({ where: { astrology_sign: ["gemini", "libra", "aquarius"]  }}).then(function(matches) {
+        // res.render is what loads the info to the handlebars file (front-end)
+        res.render("user", {
+          userdata: dbUser,
+          matches: matches
+        });
+      });
+      
+      break;
+
+      case "aries":
+      db.User.findAll({ where: { astrology_sign: ["gemini", "libra", "aquarius"]  }}).then(function(matches) {
+        // res.render is what loads the info to the handlebars file (front-end)
+        res.render("user", {
+          userdata: dbUser,
+          matches: matches
+        });
+      });
+      
+      break;
+
+      case "cancer":
+      db.User.findAll({ where: { astrology_sign: ["taurus", "virgo", "capricorn"]  }}).then(function(matches) {
+        // res.render is what loads the info to the handlebars file (front-end)
+        res.render("user", {
+          userdata: dbUser,
+          matches: matches
+        });
+      });
+      
+      break;
+      case "scorpio":
+      db.User.findAll({ where: { astrology_sign: ["taurus", "virgo", "capricorn"]  }}).then(function(matches) {
+        // res.render is what loads the info to the handlebars file (front-end)
+        res.render("user", {
+          userdata: dbUser,
+          matches: matches
+        });
+      });
+      
+      break;
+      case "pisces":
+      db.User.findAll({ where: { astrology_sign: ["taurus", "virgo", "capricorn"]  }}).then(function(matches) {
+        // res.render is what loads the info to the handlebars file (front-end)
+        res.render("user", {
+          userdata: dbUser,
+          matches: matches
+        });
+      });
+      
+      break;
+
+      case "taurus":
+      db.User.findAll({ where: { astrology_sign: ["cancer", "scorpio", "pisces"]  }}).then(function(matches) {
+        // res.render is what loads the info to the handlebars file (front-end)
+        res.render("user", {
+          userdata: dbUser,
+          matches: matches
+        });
+      });
+      
+      break;
+      case "virgo":
+      db.User.findAll({ where: { astrology_sign: ["cancer", "scorpio", "pisces"]  }}).then(function(matches) {
+        // res.render is what loads the info to the handlebars file (front-end)
+        res.render("user", {
+          userdata: dbUser,
+          matches: matches
+        });
+      });
+      
+      break;
+      case "capricorn":
+      db.User.findAll({ where: { astrology_sign: ["cancer", "scorpio", "pisces"]  }}).then(function(matches) {
+        // res.render is what loads the info to the handlebars file (front-end)
+        res.render("user", {
+          userdata: dbUser,
+          matches: matches
+        });
+      });
+      
+      break;
+      case "gemini":
+      db.User.findAll({ where: { astrology_sign: ["aries", "leo", "sagittarius"]  }}).then(function(matches) {
+        // res.render is what loads the info to the handlebars file (front-end)
+        res.render("user", {
+          userdata: dbUser,
+          matches: matches
+        });
+      });
+      
+      break;
+      case "libra":
+      db.User.findAll({ where: { astrology_sign: ["aries", "leo", "sagittarius"]  }}).then(function(matches) {
+        // res.render is what loads the info to the handlebars file (front-end)
+        res.render("user", {
+          userdata: dbUser,
+          matches: matches
+        });
+      });
+      
+      break;
+
+      case "sagittarius":
+      db.User.findAll({ where: { astrology_sign: ["gemini", "libra", "aquarius"]  }}).then(function(matches) {
+        // res.render is what loads the info to the handlebars file (front-end)
+        res.render("user", {
+          userdata: dbUser,
+          matches: matches
+        });
+      });
+      
+      break;
+      
+
     }    
 
     });
