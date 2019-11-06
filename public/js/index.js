f5753f6889bc3b67e0ba8a7f058b88eb4a664422
Maliah Lacy <mlacy0915@gmail.com>
2019-11-06 08:32:17 -0600

    fixed submit button link in index

--- public/js/index.js
+++ public/js/index.js
@@ -97,3 +97,30 @@ var handleDeleteBtnClick = function() {
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
 $exampleList.on("click", ".delete", handleDeleteBtnClick);
+
+//  404 error page
+
+const title = document.querySelector('.error_title')
+
+document.onmousemove = function(e) {
+  let x = e.pageX - window.innerWidth/2;
+  let y = e.pageY - window.innerHeight/2;
+  
+  title.style.setProperty('--x', x + 'px')
+  title.style.setProperty('--y', y + 'px')
+}
+
+title.onmousemove = function(e) {
+  let x = e.pageX - window.innerWidth/2;
+  let y = e.pageY - window.innerHeight/2;
+
+  let rad = Math.atan2(y, x).toFixed(2); 
+  let length = Math.round(Math.sqrt((Math.pow(x,2))+(Math.pow(y,2)))/10); 
+
+  let x_shadow = Math.round(length * Math.cos(rad));
+  let y_shadow = Math.round(length * Math.sin(rad));
+
+  title.style.setProperty('--x-shadow', - x_shadow + 'px')
+  title.style.setProperty('--y-shadow', - y_shadow + 'px')
+
+}
